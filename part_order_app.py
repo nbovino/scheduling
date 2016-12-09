2df104bd0b3a3091271e8dd0e6cb7292dff0e65b
Nate <nbovino05@gmail.com>
2016-12-09 11:14:26 -0500

    part order app started

--- part_order_app.py
+++ part_order_app.py
@@ -13,3 +13,12 @@ from dateutil.parser import parse
 conn = db_connect.connect_to_db()
 cur = conn.cursor()
 
+def add_new_part():
+    return
+
+def new_part_order():
+    return
+
+def place_part():
+    return
+
